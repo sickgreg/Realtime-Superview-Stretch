f091a8ad21492a158d0c869b91edc4da45d0eaf8
sickgreg <55742743+sickgreg@users.noreply.github.com>
2020-02-03 16:25:43 +1100

    Update realtime-superview-stretch.hlsl

--- realtime-superview-stretch.hlsl
+++ realtime-superview-stretch.hlsl
@@ -1,27 +1,22 @@
 
-#define PERCENTAGE_NOT_STRETCHED 0.0
-#define INPUT_TO_OUTPUT_RATIO ( ( 4.0 / 3.0 ) / ( 16.0 / 9.0 ) )
-
-
 sampler s0 : register(s0);
 
 float4 main(float2 tex : TEXCOORD0) : COLOR
 {
-    tex.x = tex.x * 2.0 - 1.0;
+    
+// convert incoming pixel coord from a value 0 to 1 to a value -1 to +1
+	tex.x = tex.x * 2.0 - 1.0;
 
-    float stretchThreshold = INPUT_TO_OUTPUT_RATIO * PERCENTAGE_NOT_STRETCHED;
+// give pixel its distanceFromEdge value between 0 and 1
+	float distanceFromEdge = max( abs(tex.x ), 0.0 ) * 0.5 + 0.5 ;
 
-    float distanceFromEdge = max( ( abs( tex.x ) - stretchThreshold ) / ( 1.0 - stretchThreshold ), 0.0 );
-
-    tex.x = lerp( tex.x / INPUT_TO_OUTPUT_RATIO, tex.x, (distanceFromEdge * 0.5 + 0.5 )*  (distanceFromEdge * 0.5 + 0.5 )   );
-
+// linear interpolate using the square of the distance from the edge
+    tex.x = lerp( tex.x / 0.75, tex.x, pow(distanceFromEdge,2)  );
  
+// convert outgoing pixel coord from a value -1 to +1 back to a value 0 to 1
     tex.x = tex.x * 0.5 + 0.5;
 
-
+//return the pixel
     float4 l = tex2D(s0, tex);
-
     return l;
 }
-
-
